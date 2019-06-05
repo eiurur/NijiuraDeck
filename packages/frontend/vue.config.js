72b2766b4e3c36805fa7492ff719ed8862698c04
eiurur <sigsig_kbeatodenmania@yahoo.co.jp>
2019-06-05 22:43:45 +0900

    開発用サーバのプロキシの設定を追加

--- packages/frontend/vue.config.js
+++ packages/frontend/vue.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:12700/'
+      }
+    },
     host: '127.0.0.1',
     port: 9001 // CHANGE YOUR PORT HERE!
     // https: false,
