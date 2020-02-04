a60f560aa9dd80d6f16f8e2be3d25d875ced2df3
eiurur <sigsig_kbeatodenmania@yahoo.co.jp>
2020-02-04 20:13:54 +0900

    fix typo

--- packages/frontend/src/components/ResponseList.vue
+++ packages/frontend/src/components/ResponseList.vue
@@ -21,11 +21,7 @@
               <div class="response-header">
                 <div class="left">
                   <span>No.{{ id }}</span>
-                  <span
-                    class="clickable address"
-                    v-if="address"
-                    @click="copyToClipboard(address)"
-                  >
+                  <span class="clickable address" v-if="address" @click="copyToClipboard(address)">
                     <el-tooltip effect="dark" placement="bottom">
                       <div slot="content">{{ address }}</div>
                       <i class="el-icon-message"></i>
@@ -40,11 +36,7 @@
                 <div class="res" ref="res" v-html="$sanitize(rawText)"></div>
               </div>
             </div>
-            <ResponseImage
-              :thumb="thumb"
-              :img="img"
-              :orig="img"
-            ></ResponseImage>
+            <ResponseImage :thumb="thumb" :img="img" :orig="img"></ResponseImage>
           </div>
         </div>
       </article>
@@ -121,14 +113,14 @@ article.stream-item {
 </style>
 
 <script>
-import ResponseImage from '@/components/ResponseImage.vue'; // @ is an alias to /src
+import ResponseImage from "@/components/ResponseImage.vue"; // @ is an alias to /src
 
 export default {
-  name: 'ResponseList',
+  name: "ResponseList",
   components: {
-    ResponseImage,
+    ResponseImage
   },
-  props: ['responses', 'isFilteringImage'],
+  props: ["responses", "isFilteringImage"],
   computed: {
     filteredResponse() {
       if (!this.responses || this.responses.length === 0) {
@@ -140,18 +132,18 @@ export default {
         }
         return true;
       });
-    },
+    }
   },
   mounted() {
     if (!this.$refs.res) return;
     Array.from(this.$refs.res).map(r => {
-      const nextTheadLinks = r.querySelectorAll('span[data-id]');
-      if (nextTheadLinks.length === 0) return;
-      Array.from(nextTheadLinks).map(l => {
-        l.addEventListener('click', event => {
-          const threadID = event.target.getAttribute('data-id');
-          const payload = { boardType: 'MAY', id: threadID };
-          this.$store.dispatch('watchingThread/load', payload);
+      const nextThreadLinks = r.querySelectorAll("span[data-id]");
+      if (nextThreadLinks.length === 0) return;
+      Array.from(nextThreadLinks).map(l => {
+        l.addEventListener("click", event => {
+          const threadID = event.target.getAttribute("data-id");
+          const payload = { boardType: "MAY", id: threadID };
+          this.$store.dispatch("watchingThread/load", payload);
         });
       });
     });
@@ -161,13 +153,13 @@ export default {
       navigator.clipboard.writeText(text).then(() => {
         this.$message({
           message: `クリップボードにコピーしました: ${text}`,
-          type: 'success',
+          type: "success"
         });
       });
     },
     moveToBottom() {
       this.$refs.scrollable.scrollTop = this.$refs.scrollable.scrollHeight;
-    },
-  },
+    }
+  }
 };
 </script>
