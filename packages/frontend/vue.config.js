231b8075b1b1ebeaedc71bc615be3b434b0f9348
eiurur <sigsig_kbeatodenmania@yahoo.co.jp>
2020-03-23 22:29:08 +0900

    Fix - was compiled against a different Node.js version using iconv

--- packages/frontend/vue.config.js
+++ packages/frontend/vue.config.js
@@ -2,10 +2,10 @@ module.exports = {
   devServer: {
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:12701/'
-      }
+        target: 'http://127.0.0.1:12701/',
+      },
     },
     host: '127.0.0.1',
-    port: 9001
-  }
+    port: 9001,
+  },
 };
