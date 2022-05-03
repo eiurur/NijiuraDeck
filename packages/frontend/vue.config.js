43fa6d79c47bf136064dbe1b456d93d22d73ba90
eiurur <eiurur789@gmail.com>
2022-05-04 04:46:57 +0900

    update proxy configure

--- packages/frontend/vue.config.js
+++ packages/frontend/vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   devServer: {
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:12701/',
+        target: 'http://127.0.0.1:8701/',
       },
     },
     host: '127.0.0.1',
